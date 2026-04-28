3217210a4e426797620ce68bd28a2d671dfc36d9
Антон Кузянин <marswaitsfor@gmail.com>
2026-04-28 22:46:44 +0300

    fix for checkstyle

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -16,4 +16,9 @@ public class Url {
     private Long id;
     private String name;
     private Timestamp createdAt;
+
+    public Url(String name) {
+        this.name = name;
+        this.createdAt = new Timestamp(System.currentTimeMillis());
+    }
 }
