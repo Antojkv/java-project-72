aea31b923a0fbd2c1bf7571d44b921976749713b
Антон Кузянин <marswaitsfor@gmail.com>
2026-07-16 23:42:25 +0300

    fix hexlet test

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS urls (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS url_checks (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     url_id BIGINT NOT NULL,
     status_code INTEGER,
     h1 VARCHAR(255),
@@ -13,4 +13,4 @@ CREATE TABLE IF NOT EXISTS url_checks (
     description TEXT,
     created_at TIMESTAMP NOT NULL,
     FOREIGN KEY (url_id) REFERENCES urls(id) ON DELETE CASCADE
-    );
+);
